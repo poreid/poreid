03a918151b65f68dc53b880b6f95ea54389125f3
jporeid@gmail.com <jporeid@gmail.com@d40f1747-1ce4-a232-dfcd-879350b70c81>
2014-02-27 09:16:14 +0000

    [maven-release-plugin]  copy for tag poreid-1.0

---
+++ tags/poreid-1.0/src/main/java/org/poreid/SecurityStatusNotSatisfiedException.java
@@ -0,0 +1,39 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2014 Rui Martinho (rmartinho@gmail.com), António Braz (antoniocbraz@gmail.com)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+package org.poreid;
+
+/**
+ * Exceção lançada quando existe uma tentativa de utilização de um recurso protegido no cartão
+ * @author POReID
+ */
+public class SecurityStatusNotSatisfiedException extends Exception{
+
+    /**
+     * Constroi uma nova exceção com a mensagem de detalhe
+     * @param detalhe
+     */
+    public SecurityStatusNotSatisfiedException(String detalhe) {
+        super(detalhe);
+    }
+}
